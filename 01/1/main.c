20d5bbf84692a8d3e61d4b034530aebcdc2ba938
isiko <koerner@informatik-ag.net>
2021-12-02 23:07:54 +0100

    Improved Code for Day 1

--- 01/1/main.c
+++ 01/1/main.c
@@ -4,32 +4,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define inputLength 2000
+int input[inputLength];
+
+int readFile(){
+    FILE *input_file;
+    input_file = fopen("../input.txt", "r");
+
+    if (!input_file){
+        printf("Error while loading File");
+        return(0);
+    }
+
+    for(int i = 0; i<inputLength; i++){
+        char line[256]; /* or other suitable maximum line size */
+        if (fgets(line, sizeof line, input_file) != NULL)
+            input[i] = atoi(line);
+    }
+
+    printf("Test");
+    fclose(input_file);
+    return (1);
+}
+
+#define startingIndex 1
+
 int main() {
-    int counter = 0;
-    int numbers[2000];
-    int startingIndex = 1;
+    if(readFile()){
+        int counter = 0;
 
-    FILE *input;
-    input = fopen("../input.txt", "r");
+        int lastInt = 0;
+        for (int i = startingIndex; i < inputLength; i++) {
+            int current = input[i];
+            printf("%d\n", current);
+            if (i != startingIndex && current > lastInt) counter++;
+            lastInt = current;
+        }
 
-    int i = 0;
-    char line[256]; /* or other suitable maximum line size */
-    while (fgets(line, sizeof line, input) != NULL){
-        numbers[i] = atoi(line);
-        i++;
+        printf("%d Erhoehungen", counter);
+        return 0;
     }
 
-    int lastInt = 0;
-    for (int i = startingIndex; i<2000; i++){
-        int current = numbers[i];
-        printf("%d\n", current);
-        if (i != startingIndex && current > lastInt) counter++;
-        lastInt = current;
-    }
-    fclose(input);
-
-    printf("%d Erhoehungen", counter);
-
-    return 0;
+    return 1;
 }
 
--- 01/2/main.c
+++ 01/2/main.c
@@ -4,31 +4,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define inputLength 2000
+int input[inputLength];
+
+int readFile(){
+    FILE *input_file;
+    input_file = fopen("../input.txt", "r");
+
+    if (!input_file){
+        printf("Error while loading File");
+        return(0);
+    }
+
+    for(int i = 0; i<inputLength; i++){
+        char line[256]; /* or other suitable maximum line size */
+        if (fgets(line, sizeof line, input_file) != NULL)
+            input[i] = atoi(line);
+    }
+
+    printf("Test");
+    fclose(input_file);
+    return (1);
+}
+
 int main() {
-    int counter = 0;
-    int numbers[2000];
+    if (readFile()) {
+        int counter = 0;
+        int lastInt = 0;
 
-    FILE *input;
-    input = fopen("../input.txt", "r");
+        for (int i = 2; i < 2000; i++) {
+            int sum = input[i] + input[i - 1] + input[i - 2];
+            printf("%d\n", sum);
+            if (i != 2 && sum > lastInt) counter++;
+            lastInt = sum;
+        }
+        printf("%d Erhoehungen", counter);
 
-    int i = 0;
-    char line[256]; /* or other suitable maximum line size */
-    while (fgets(line, sizeof line, input) != NULL){
-        numbers[i] = atoi(line);
-        i++;
+        return 0;
     }
-
-    int lastInt = 0;
-    for (int i = 2; i<2000; i++){
-        int sum = numbers[i] + numbers[i-1] + numbers[i-2];
-        printf("%d\n", sum);
-        if (i != 2 && sum > lastInt) counter++;
-        lastInt = sum;
-    }
-    fclose(input);
-
-    printf("%d Erhoehungen", counter);
-
-    return 0;
 }
 
