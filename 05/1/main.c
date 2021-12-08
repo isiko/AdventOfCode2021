939430331b57def2557917cedb5c2782c5f869b4
isiko <koerner@informatik-ag.net>
2021-12-08 10:16:57 +0100

    Solved Day 5

---
+++ 05/1/main.c
@@ -0,0 +1,135 @@
+//
+// Created by isihd on 03.12.2021.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define inputLength 500
+
+int lines[inputLength][2][2];
+int map[1000][1000];
+
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
+
+void markLinePoints(int start[], int end[]){
+    
+    if(start[0] == end[0] || start[1] == end[1]){
+        int vel[2];
+        for(int i = 0; i<2; i++){
+            if(start[i] != end[i])
+                if(start[i] < end[i])
+                    vel[i] = 1;
+                else
+                    vel[i] = -1;
+            else
+                vel[i] = 0;
+        }
+
+        printf("Starting Line at %d:%d\n", start[0], start[1]);
+        map[start[0]][start[1]]++;
+        while ((start[0] != end[0] || start[1] != end[1])){
+            for(int j = 0; j<2; j++)
+                start[j] += vel[j];
+            map[start[0]][start[1]]++;
+            //printf("(%d:%d) -> %d\n", start[0], start[1], map[start[0]][start[1]]);
+        }
+        printf("Ending Line at %d:%d\n", start[0], start[1]);
+    }
+}
+
+int main(){
+    FILE *input_file;
+    input_file = fopen("../input.txt", "r");
+
+    if (!input_file){
+        printf("Error while loading File\n");
+        return(0);
+    }
+
+    char line[512];
+
+    for(int x = 0; x<1000; x++){
+        for(int y = 0; y<1000; y++){
+            map[x][y] = 0;
+        }
+    }
+
+    for (int i = 0; i < inputLength; i++)
+    {
+        fgets(line, sizeof line, input_file);
+        line[strcspn(line, "\n")] = 0;
+        char** lineCords = str_split(line, ';');
+        char** start = str_split(lineCords[0], ',');
+        char** end = str_split(lineCords[1], ',');
+
+        //Start
+        lines[i][0][0] = atoi(start[0]);
+        lines[i][0][1] = atoi(start[1]);
+
+        //End
+        lines[i][1][0] = atoi(end[0]);
+        lines[i][1][1] = atoi(end[1]);
+
+        printf("%d:\t%d,%d -> %d,%d\n",i, lines[i][0][0], lines[i][0][1], lines[i][1][0], lines[i][1][1]);
+        markLinePoints(lines[i][0], lines[i][1]);
+        printf("\n");
+    }
+
+    int result = 0;
+    for (int x = 0; x < 1000; x++)
+        for (int y = 0; y < 1000; y++)
+            if (map[x][y] >= 2)
+                result++;
+    
+    printf("Result: %d", result);
+
+    return 0;
+}
