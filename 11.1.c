18e148da7719e891fadf99e7e1157ce324222bca
isiko <koerner@informatik-ag.net>
2021-12-11 18:15:16 +0100

    Solved Day 11

---
+++ 11.1.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "util.c"
+
+#define inputLength 10
+#define inputHeight 10
+
+int input[inputHeight][inputLength];
+int flashed[inputHeight][inputLength];
+int flashes = 0;
+
+int readFile(){
+    FILE *input_file;
+    input_file = fopen("../../inputs/11.txt", "r");
+
+    if (!input_file){
+        printf("Error while loading File");
+        return(0);
+    }
+
+    char line[(inputLength+1)*inputHeight]; /* or other suitable maximum line size */
+    fgets(line, sizeof line, input_file);
+    //line[strcspn(line, "\n")] = 0;
+    char** inputChars = str_split(line, ';');
+    for(int i = 0; i<inputHeight; i++){
+        for(int j = 0; j<inputLength; j++){
+            input[i][j] = inputChars[i][j] - '0';
+            flashed[i][j] = 0;
+        }
+    }
+
+    // fclose(input_file);
+    return (1);
+}
+
+void printBoard(){
+    printf("\n");
+    printf("%d Flashes\n", flashes);
+    for (int i = 0; i<inputHeight; i++){
+        for (int j = 0; j < inputLength; j++)
+        {
+            printf("%d", input[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void checkXY(int x, int y, int initalFlash){
+    if(initalFlash == 0) input[x][y]++;
+    
+    if(flashed[x][y] == 0 && input[x][y] > 9){
+        
+        flashes++;
+        flashed[x][y] = 1;
+        
+        if(x>0) checkXY(x-1, y, 0);
+        if(y>0) checkXY(x, y-1, 0);
+        if(x<inputHeight-1) checkXY(x+1, y, 0);
+        if(y<inputLength-1) checkXY(x, y+1, 0);
+
+        if(y>0 && x>0) checkXY(x-1, y-1, 0);
+        if(x<inputHeight-1 && y>0) checkXY(x+1, y-1, 0);
+        if(y<inputLength-1 && x>0) checkXY(x-1, y+1, 0);
+        if(y<inputLength-1 && x<inputHeight-1) checkXY(x+1, y+1, 0);
+    } else {
+
+    }
+}
+
+int main(){
+    if(readFile()){
+        for(int iteration = 0; iteration<100; iteration++){
+            // printBoard();
+            for (int i = 0; i<inputHeight; i++){
+                for (int j = 0; j < inputLength; j++)
+                {
+                    input[i][j]++;
+                }
+            }
+
+            for (int i = 0; i<inputHeight; i++){
+                for (int j = 0; j < inputLength; j++)
+                {
+                    if(input[i][j] == 10 && flashed[i][j] == 0) checkXY(i,j, 1);
+                }
+            }
+
+            for (int i = 0; i<inputHeight; i++){
+                for (int j = 0; j < inputLength; j++)
+                {
+                    if(input[i][j] > 9) input[i][j] = 0;
+                    flashed[i][j] = 0;
+                }
+            }
+        }
+        printBoard();
+    }
+}
---
+++ 11.2.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "util.c"
+
+#define inputLength 10
+#define inputHeight 10
+
+int input[inputHeight][inputLength];
+int flashed[inputHeight][inputLength];
+int flashes = 0;
+
+int readFile(){
+    FILE *input_file;
+    input_file = fopen("../../inputs/11.txt", "r");
+
+    if (!input_file){
+        printf("Error while loading File");
+        return(0);
+    }
+
+    char line[(inputLength+1)*inputHeight]; /* or other suitable maximum line size */
+    fgets(line, sizeof line, input_file);
+    //line[strcspn(line, "\n")] = 0;
+    char** inputChars = str_split(line, ';');
+    for(int i = 0; i<inputHeight; i++){
+        for(int j = 0; j<inputLength; j++){
+            input[i][j] = inputChars[i][j] - '0';
+            flashed[i][j] = 0;
+        }
+    }
+
+    // fclose(input_file);
+    return (1);
+}
+
+void printBoard(){
+    printf("\n");
+    printf("%d Flashes\n", flashes);
+    for (int i = 0; i<inputHeight; i++){
+        for (int j = 0; j < inputLength; j++)
+        {
+            printf("%d", input[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void checkXY(int x, int y, int initalFlash){
+    if(initalFlash == 0) input[x][y]++;
+    
+    if(flashed[x][y] == 0 && input[x][y] > 9){
+        
+        flashes++;
+        flashed[x][y] = 1;
+        
+        if(x>0) checkXY(x-1, y, 0);
+        if(y>0) checkXY(x, y-1, 0);
+        if(x<inputHeight-1) checkXY(x+1, y, 0);
+        if(y<inputLength-1) checkXY(x, y+1, 0);
+
+        if(y>0 && x>0) checkXY(x-1, y-1, 0);
+        if(x<inputHeight-1 && y>0) checkXY(x+1, y-1, 0);
+        if(y<inputLength-1 && x>0) checkXY(x-1, y+1, 0);
+        if(y<inputLength-1 && x<inputHeight-1) checkXY(x+1, y+1, 0);
+    } else {
+
+    }
+}
+
+int main(){
+    if(readFile()){
+        for(int iteration = 0; 1; iteration++){
+            // printBoard();
+            for (int i = 0; i<inputHeight; i++){
+                for (int j = 0; j < inputLength; j++)
+                {
+                    input[i][j]++;
+                }
+            }
+
+            for (int i = 0; i<inputHeight; i++){
+                for (int j = 0; j < inputLength; j++)
+                {
+                    if(input[i][j] == 10 && flashed[i][j] == 0) checkXY(i,j, 1);
+                }
+            }
+
+            int stepFlashCounter = 0;
+            for (int i = 0; i<inputHeight; i++){
+                for (int j = 0; j < inputLength; j++)
+                {
+                    if(input[i][j] > 9) {
+                        input[i][j] = 0;
+                        stepFlashCounter++;
+                    }
+                    flashed[i][j] = 0;
+                }
+            }
+            if(stepFlashCounter >= inputHeight*inputLength){
+                printf("Everything synced at Step %d\n", iteration+1);
+                break;
+            }
+        }
+        printBoard();
+    }
+}
